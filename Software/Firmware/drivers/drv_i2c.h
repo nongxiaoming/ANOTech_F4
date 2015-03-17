740556376543c70c54a96c549adb781a3544ade6
nongxiaoming <nongxiaoming@gmail.com>
2015-03-17 19:27:18 +0800

    remove the unused include.

--- Software/Firmware/drivers/drv_i2c.h
+++ Software/Firmware/drivers/drv_i2c.h
@@ -3,10 +3,6 @@
 
 #include <rtthread.h>
 #include "stm32f4xx.h"
-#include "stm32f4xx_i2c.h"  
-#include "stm32f4xx_dma.h" 
-#include "stm32f4xx_gpio.h"
-#include "stm32f4xx_rcc.h"
 #include "misc.h"
 
 
